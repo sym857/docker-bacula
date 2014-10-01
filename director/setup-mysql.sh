02b81d30dd59592fb64dec23661fb26f03d36d12
sym857 <github@satkinson.co.uk>
2014-10-01 19:29:23 +0100

    Still trying to get mysql connected

--- director/setup-mysql.sh
+++ director/setup-mysql.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-/usr/libexec/bacula/grant_mysql_privileges -u root -p$MYSQL_PASSWORD
-/usr/libexec/bacula/create_mysql_database -u root -p$MYSQL_PASSWORD
-/usr/libexec/bacula/make_mysql_tables -u root -p$MYSQL_PASSWORD
-/usr/libexec/bacula/grant_bacula_privileges -u root -p$MYSQL_PASSWORD
+/usr/libexec/bacula/grant_mysql_privileges -h $MYSQL_IPADDRESS -u root -p$MYSQL_PASSWORD
+/usr/libexec/bacula/create_mysql_database -h $MYSQL_IPADDRESS -u root -p$MYSQL_PASSWORD
+/usr/libexec/bacula/make_mysql_tables -h $MYSQL_IPADDRESS -u root -p$MYSQL_PASSWORD
+/usr/libexec/bacula/grant_bacula_privileges -h $MYSQL_IPADDRESS -u root -p$MYSQL_PASSWORD
 
 echo "use mysql;" > /mysql.sql
 echo "UPDATE user SET password=PASSWORD(\"$MYSQL_PASSWORD\") WHERE user='bacula';" >> /mysql.sql
 echo "FLUSH PRIVILEGES;" >> /mysql.sql
 echo "exit" >> /mysql.sql
 
-mysql -u root -p$MYSQL_PASSWORD < /mysql.sql
+mysql -h $MYSQL_IPADDRESS -u root -p$MYSQL_PASSWORD < /mysql.sql
 
 
  
